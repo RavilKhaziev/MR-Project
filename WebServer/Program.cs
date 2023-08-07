7a4d367a660d2508449b21ca1ff89c9a22e9036a
Khaziev Ravil <100825440+RavilKhaziev@users.noreply.github.com>
2023-08-08 02:39:29 +0500

    ch

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -7,8 +7,111 @@ using IdentityServer4.Services;
 using IdentityServer4.Models;
 using IdentityServer4;
 using WebServer;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Security;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+//{
+//	// Describe certificate
+//	string subject = "CN=localhost";
+//	var rsaKey = RSA.Create(2048);
+
+//	// Create certificate request
+//	var certificateRequest = new CertificateRequest(
+//		subject,
+//		rsaKey,
+//		HashAlgorithmName.SHA256,
+//		RSASignaturePadding.Pkcs1
+//	);
+
+//	certificateRequest.CertificateExtensions.Add(
+//		new X509BasicConstraintsExtension(
+//			certificateAuthority: false,
+//			hasPathLengthConstraint: false,
+//			pathLengthConstraint: 0,
+//			critical: true
+//		)
+//	);
+
+//	certificateRequest.CertificateExtensions.Add(
+//		new X509KeyUsageExtension(
+//			keyUsages:
+//				X509KeyUsageFlags.DigitalSignature
+//				| X509KeyUsageFlags.KeyEncipherment,
+//			critical: false
+//		)
+//	);
+
+//	certificateRequest.CertificateExtensions.Add(
+//		new X509SubjectKeyIdentifierExtension(
+//			key: certificateRequest.PublicKey,
+//			critical: false
+//		)
+//	);
+
+//	certificateRequest.CertificateExtensions.Add(
+//		new X509Extension(
+//			new AsnEncodedData(
+//				"Subject Alternative Name",
+//				new byte[] { 48, 11, 130, 9, 108, 111, 99, 97, 108, 104, 111, 115, 116 }
+//			),
+//			false
+//		)
+//	);
+
+//	var expireAt = DateTimeOffset.Now.AddYears(1);
+
+//	var certificate1 = certificateRequest.CreateSelfSigned(DateTimeOffset.Now, expireAt);
+
+//	var exportableCertificate = new X509Certificate2(
+//		certificate1.Export(X509ContentType.Cert),
+//		(string)null,
+//		X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet
+//	).CopyWithPrivateKey(rsaKey);
+
+//	var passwordForCertificateProtection = new SecureString();
+//	foreach (var @char in "p@ssw0rd")
+//	{
+//		passwordForCertificateProtection.AppendChar(@char);
+//	}
+
+//	// Export certificate to a file.
+//	File.WriteAllBytes(
+//		"certificateForServerAuthorization.pfx",
+//		exportableCertificate.Export(
+//			X509ContentType.Pfx,
+//			passwordForCertificateProtection
+//		)
+//	);
+//}
+
+//var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+//store.Open(OpenFlags.ReadOnly);
+//var certificate = store.Certificates.OfType<X509Certificate2>()
+//	.First(c => c.FriendlyName ==
+//	"Ivan Yakimov Test-only Certificate For Server Authorization");
+
+//var builder1 = Host.CreateDefaultBuilder(args)
+//			.ConfigureWebHostDefaults(webBuilder =>
+//			{
+//				webBuilder
+//					.UseKestrel(options =>
+//					{
+//						options.Listen(System.Net.IPAddress.Loopback, 44321, listenOptions =>
+//						{
+//							var connectionOptions = new HttpsConnectionAdapterOptions();
+//							connectionOptions.ServerCertificate = certificate;
+
+//							listenOptions.UseHttps(connectionOptions);
+//						});
+//					});
+//			});
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
