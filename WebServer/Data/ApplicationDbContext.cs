37255950fc9951c21abc63b5ac5bf19033d784a2
Khaziev Ravil <100825440+RavilKhaziev@users.noreply.github.com>
2023-08-08 19:58:05 +0500

    Changed

--- WebServer/Data/ApplicationDbContext.cs
+++ WebServer/Data/ApplicationDbContext.cs
@@ -8,12 +8,18 @@ namespace WebServer.Data
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
-			
+		
 		}
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			Database.MigrateAsync();
 		}
 
 	}
--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -122,9 +122,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
 	options.UseNpgsql(dataSource)
 	 .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning));
-}
-
-	);
+	
+});
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -168,10 +167,12 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
+	//app.UseExceptionHandler("/Home/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	//app.UseHsts();
 }
+
+
 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
